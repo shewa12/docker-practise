0ea994e4b6ef0e81b12d92c3dddd0d55a4a9cb69
shewa12 <shewa12kpi@gmail.com>
2022-11-09 22:36:46 +0600

    new: dockerfile added for production build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning React
+         Learn React
         </a>
       </header>
     </div>
