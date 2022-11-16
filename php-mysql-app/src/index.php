5cba1c589fea256e3fa69946efccf56c6af8dcf5
shewa12 <shewa12kpi@gmail.com>
2022-11-16 23:30:03 +0600

    New: php-mysql-app files

---
+++ php-mysql-app/src/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Docker Application</title>
+</head>
+<body>
+    <h3>Welcome to the Docker PHP Application!!</h3>
+<?php
+    require 'test.php';
+?>
+</body>
+</html>
